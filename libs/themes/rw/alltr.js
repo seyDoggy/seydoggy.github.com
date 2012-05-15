9a1a55ca697a15184ab8dc7903b8d181b7c6b7b8
Adam Merrifield <macagp@gmail.com>
2012-05-14 21:35:33 -0400

    Fixed issue adc/07a:om: ExtraContent snippets placed in empty sidebar will cause sidebar to render.

--- libs/themes/rw/alltr.js
+++ libs/themes/rw/alltr.js
@@ -151,7 +151,7 @@ jQuery(document).ready(function($){
 		================================================== */
 		var sidebar_fn = (function(){
 			// when sidebar and sidebar title is empty
-			if (!h3_sidebar_title.html().length && !div_sidebar.contents().not(h3_sidebar_title).length) div_sidebar.css('display','none');
+			if (!h3_sidebar_title.html().length && !div_sidebar.contents().not('h3#sidebar_title, div[id*="myExtraContent"]').length) div_sidebar.css('display','none');
 			else if (!h3_sidebar_title.html().length) h3_sidebar_title.css('display','none');
 			
 			// when plugin_sidebar is !empty
