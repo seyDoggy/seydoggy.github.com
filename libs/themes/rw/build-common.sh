4ebf98fb44b0df64f4a3c699a5009c3973853a44
Adam Merrifield <macagp@gmail.com>
2012-09-28 07:33:34 -0400

    Fine tuned build script.

--- libs/themes/rw/build-common.sh
+++ libs/themes/rw/build-common.sh
@@ -14,17 +14,22 @@ BUILDNUM=`date "+%y%m%d%H%M%S"`
 #############
 
 # get cycle
+echo "Getting Cycle..."
 curl https://raw.github.com/malsup/cycle/master/jquery.cycle.all.js > pull-cycle.js
+echo "Done!"
 
 # compress it
+echo "Compressing cycle..."
 jsmin <pull-cycle.js >pull-cycle.min.js
-java com.yahoo.platform.yui.compressor.Bootstrap --type js -o pull-cycle.min.js pull-cycle.min.js
+java com.yahoo.platform.yui.compressor.Bootstrap --type js pull-cycle.min.js -o pull-cycle.min.js
+echo "Done!"
 
 
 ####################
 # GET COMMON FILES #
 ####################
 
+echo "Getting common files..."
 # get RwSetGet
 cat ~/github/RwSetGet/rwget.js > pull-common.js && echo -e "\n" >> pull-common.js
 # get rwAddLinks
@@ -45,16 +50,20 @@ cat ~/github/sdLightboxAlbums/jquery.sdalbumstyle.js >> pull-common.js && echo -
 cat ~/github/IE9Gradius/jquery.ie9gradius.js >> pull-common.js && echo -e "\n" >> pull-common.js
 # get Frehmwerk
 cat ~/github/seydoggy.github.com/libs/themes/rw/frehmwerk.js >> pull-common.js && echo -e "\n" >> pull-common.js
+echo "Done!"
 
 
 # compress pull-common.js
+echo "Compressing common..."
 jsmin <pull-common.js >pull-common.min.js
-java com.yahoo.platform.yui.compressor.Bootstrap --type js -o pull-common.min.js pull-common.min.js
-
+java com.yahoo.platform.yui.compressor.Bootstrap --type js pull-common.min.js -o pull-common.min.js
+echo "Done!"
 
 ##################################
 # COMBINE CYCLE and COMMON FILES #
 ##################################
+
+echo "Combining common and Cycle files..."
 
 echo -e "\n\n/*\n\tjQuery Cycle Plugin\n\thttp://jquery.malsup.com/license.html\n*/\n" > pull.js
 cat pull-cycle.min.js >> pull.js
@@ -63,10 +72,13 @@ cat pull-cycle.min.js >> pull.js
 echo -e "\n\n/*\n\tseyDoggy RapidWeaver Common\n\t(c) 2012 Adam Merrifield\n\thttps://github.com/seyDoggy/\n*/\n" >> pull.js
 cat pull-common.min.js >> pull.js
 
+echo "Done!"
 
 #######################
 # BUILD COMMON.MIN.JS #
 #######################
+
+echo "Writting final common.min.js..."
 
 # write build date to common.min.js
 echo -e "/*\n\tConsolidated common scripts for seydesign/frehmwerk themes\n\t\n\tIncludes jQuery, Cycle and seyDoggy RapidWeaver Common scripts\n\tBuilt on $BUILDDATE (build No. $BUILDNUM)\n*/\n" > common.min.js
@@ -80,22 +92,31 @@ curl https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js >> common.min.
 # add contents of pull.js to common.min.js
 cat pull.js >> common.min.js
 
+echo "Done!"
+
 ###########
 # CLEANUP #
 ###########
 
+echo "Cleaning up temp files..."
+
 # remove pull*.js
 rm -rf pull*.js
 
+echo "Done!"
 
 ###########
 # PUBLISH #
 ###########
+
+echo "Publishing the results..."
 
 # commit and push changes
 git commit common.min.js -m "[build] common.min.js: new build of common scripts. (build No. $BUILDNUM)" && git push origin master
 
 # put common.min.js on s3
 s3cmd put common.min.js s3://seydesign/rw/ -m application/javascript -P --add-header=Cache-Control:max-age=0
+
+echo "Done!"
 
 exit 0
