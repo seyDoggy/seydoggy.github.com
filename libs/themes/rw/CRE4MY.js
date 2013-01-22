71f727254f5bc5ff8e7a01d276829ae1cc2d2e16
Adam Merrifield <macagp@gmail.com>
2013-01-22 09:59:10 -0500

    CRE4MY: Fixed issue ae5/265: Some Navigation types won't render responsive toolbar.

--- libs/themes/rw/CRE4MY.js
+++ libs/themes/rw/CRE4MY.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
 		var jq = $([]),
 		wHeader = jq.add('div.wrapperHeader'),
 		hContainer = wHeader.find('div.headerContainer'),
-	    sdSlideshow = hContainer.find('div.seydoggySlideshow'),
+		sdSlideshow = hContainer.find('div.seydoggySlideshow'),
 		slideHeader = sdSlideshow.add(sdSlideshow.find('div.pageHeader')),
 		siteHeader = wHeader.find('header.siteHeader'),
 		logoImg = siteHeader.find('a.logo img'),
@@ -36,8 +36,8 @@ jQuery(document).ready(function($){
 		dropVal = true;
 		
 		if (hContainer.css('display') == 'none') isNoHeader = true;
-		if (sdNav.position == 1) radiusClass = "radiusBottom";
-		else if (emptyBar == true) radiusClass = "radiusAll";
+		if (sdNav.position === 1) radiusClass = "radiusBottom";
+		else if (emptyBar === true) radiusClass = "radiusAll";
 		
 		if (jq.add('body').width() <= '600' && jq.add('meta[name=viewport]').length) dropVal = false;
 		
@@ -50,12 +50,12 @@ jQuery(document).ready(function($){
 		});
 		// set height .mainContent to that of .sidebarContent if shorter
 		if (sContent.css('display') != 'none') mContent.css('min-height',sContent.height());
-		/* @end */
+		
 
 		/* @group toolbar split/vertical options */
 		var sdNavOptions = (function(){
 			// test for legacy CRE4MY v4.7.0 (r7)
-			if (!$('div#extraContainer12').length || sdNav.drop == false) {
+			if (!$('div#extraContainer12').length || sdNav.drop === false) {
 				dropVal = false;
 			}
 			// invoke sdSmartNav
@@ -66,9 +66,6 @@ jQuery(document).ready(function($){
 			// $.sdSmartNav();
 			// if mobile
 			if (jq.add('body').width() <= '600' && jq.add('meta[name=viewport]').length) {
-				// remove additional tiers
-				sdNav.tb2.remove();
-				sdNav.tb3.remove();
 				// add link to navigation
 				$('<a href="#toolbar_vertical" title="menu" class="responsiveMenu"></a>').prependTo(siteHeader).css({
 					"background-image":"url(http://seydoggy.github.com/libs/themes/rw/plus.black.32.png)",
@@ -80,7 +77,10 @@ jQuery(document).ready(function($){
 					"width":"32px"
 				});
 				// move nav after footer
-				sdNav.tb1.insertAfter(wFooter).attr('id','toolbar_vertical').css('margin-top','1.5em').addClass('radiusAll');
+				sdNav.tb1.insertAfter(wFooter).attr('id','toolbar_vertical').css({
+					'margin-top':'1.5em',
+					'display':'block'
+				}).addClass('radiusAll');
 				// capture first and last items for toolbar_vertical
 				if (sdNav.tb1.find('ul li').length <= 1) sdNav.tb1.find('a').addClass('radiusAll');
 				else sdNav.tb1.find('ul:first li:first a:first').addClass('radiusTop')
@@ -89,15 +89,18 @@ jQuery(document).ready(function($){
 				if ($('nav#toolbar_vertical').length) {
 					var nav_toolbar3 = jq.add('nav#toolbar_vertical');
 					if (nav_toolbar3.find(" > ul li > ul")) {
-						nav_toolbar3.find("a.current").siblings("ul").css("display", "block").end().parents("ul").css("display", "block")
+						nav_toolbar3.find("a.current").siblings("ul").css("display", "block").end().parents("ul").css("display", "block");
 					}
 				}
 				// round mPreContent
 				mPreContent.addClass('radiusTop');
 				// set sub nav as empty
 				emptySub = true;
+				// remove additional tiers
+				sdNav.tb2.remove();
+				sdNav.tb3.remove();
 			} else {
-				if ((sdNav.type == 1 || sdNav.type == 3 || sdNav.type == 4) || (sdNav.tbsP == undefined)) emptySub = true;
+				if ((sdNav.type === 1 || sdNav.type === 3 || sdNav.type === 4) || (sdNav.tbsP === undefined)) emptySub = true;
 				if (!(logoImg.length) && !(siteTitle.html().length) && !(siteSlogan.html().length) && sdNav.type >= 2) emptyBar = true;
 
 				// capture first and last items for toolbar_vertical
@@ -107,7 +110,7 @@ jQuery(document).ready(function($){
 
 				// STYLES FOR TIER 2
 				// round first list item
-				if (sdNav.tbsP != undefined) sdNav.tb2.find('> ul:first > li:first >  a:first').addClass('radiusTopLeft');
+				if (sdNav.tbsP !== undefined) sdNav.tb2.find('> ul:first > li:first >  a:first').addClass('radiusTopLeft');
 				// round mPreContent
 				else mPreContent.addClass('radiusTop');
 				// style toolbar 1 & 2 drop menu
@@ -127,8 +130,7 @@ jQuery(document).ready(function($){
 				});
 			}
 		})();
-		/* @end */
-
+		
 		/* @group title vertical alignment */
 		var sdTitleValign = (function(){
 			if (!(siteTitle.html().length && siteSlogan.html().length)) {
@@ -140,8 +142,7 @@ jQuery(document).ready(function($){
 			// vertical center logo
 			if (logoImg.length) logoImg.sdVertAlign(siteHeader);
 		})();
-		/* @end */
-
+		
 		/* @group ExtraContent */
 		var sdExtracontent = (function(){
 			// EC 1 is handled in SS3
@@ -172,25 +173,25 @@ jQuery(document).ready(function($){
 			if (jq.add('div#myExtraContent12').length) {
 				jq.add('div#myExtraContent12 script').remove();
 				jq.add('div#myExtraContent12').appendTo('div#extraContainer12').show();
-			};
+			}
 
 			// dynamically style depending on EC areas used
-			if ((thisEC[3].length && thisEC[4].length) || (emptySub == false && thisEC[4].length)) cContainer.removeClass('radiusAll').css({'border-top-style':'none','border-bottom-style':'none'});
-			else if (thisEC[3].length || emptySub == false) cContainer.removeClass('radiusAll').addClass('radiusBottom').css({'border-top-style':'none'});
+			if ((thisEC[3].length && thisEC[4].length) || (emptySub === false && thisEC[4].length)) cContainer.removeClass('radiusAll').css({'border-top-style':'none','border-bottom-style':'none'});
+			else if (thisEC[3].length || emptySub === false) cContainer.removeClass('radiusAll').addClass('radiusBottom').css({'border-top-style':'none'});
 			else if (thisEC[4].length) cContainer.removeClass('radiusAll').addClass('radiusTop').css({'border-bottom-style':'none'});
-		    if (thisEC[6].length) copyright.removeClass('radiusAll').addClass('radiusBottom'), bContainer.find('> ul > li:last >  a').addClass('radiusBottomRight');
-		    else bContainer.find('> ul > li:last >  a').addClass('radiusRight');
+			if (thisEC[6].length) copyright.removeClass('radiusAll').addClass('radiusBottom'), bContainer.find('> ul > li:last >  a').addClass('radiusBottomRight');
+			else bContainer.find('> ul > li:last >  a').addClass('radiusRight');
 		})();
-		/* @end */
+		
 		
 		/* @group rounding */
 		var sdRounding = (function(){
 			// header/toolbar rounding
 			// !round top of #toolbar_horizontal if header is used
-			if (isNoHeader == false) {
+			if (isNoHeader === false) {
 				// handle the title bar when all elements are empty
-				if (emptyBar == false) {
-					if (sdNav.position == 0) {
+				if (emptyBar === false) {
+					if (sdNav.position === 0) {
 						// remove rads from title bar
 						siteHeader.removeClass('radiusAll').addClass('radiusBottom');
 						// round lower right corner of last link
@@ -211,29 +212,29 @@ jQuery(document).ready(function($){
 					siteHeader.hide();
 				}
 			} else {
-				if (emptyBar == false) {
+				if (emptyBar === false) {
 					// round both right corners of last link
 					if (sdNav.type <= 1) sdNav.tb1.find('> ul:first > li:last >  a:last').addClass('radiusRight');
 				} else siteHeader.hide();// handle the title bar when all elements are empty
 			}
 			// content round
-			if (emptySub == false) mPreContent.css('border-top-style','none');
-		    // styles for copyright/breadcrumb
-		    // show copyright area if footer or breadcrumb is present
-		    if (footer.html().length || bContainer.length) {
+			if (emptySub === false) mPreContent.css('border-top-style','none');
+			// styles for copyright/breadcrumb
+			// show copyright area if footer or breadcrumb is present
+			if (footer.html().length || bContainer.length) {
 				copyright.css('display','block'), fContainer.css('border-bottom-style','none');
 				// if both occur then position footer
-			    if (footer.html().length && bContainer.length) footer.css({'float': 'left','text-align': 'left'});
+				if (footer.html().length && bContainer.length) footer.css({'float': 'left','text-align': 'left'});
 			}
-		    // hide empty copyright area and round footer
-		    else fContainer.removeClass('radiusTop').addClass('radiusAll');
+			// hide empty copyright area and round footer
+			else fContainer.removeClass('radiusTop').addClass('radiusAll');
 		})();
-		/* @end */
+		
 
 		/* @group various page style */
 		// set width/height of File Sharing blocks according to available space
 		if (fsI.length) fsI.width(fsI.parent().outerWidth(true) / 3 - 45).addClass('radiusAll contentShadow').sdSetHeight(fsI,0);
-		/* @end */
+		
 		
 		/* @group Album */
 		var sdAlbums = (function(){
@@ -250,6 +251,6 @@ jQuery(document).ready(function($){
 				plusClass : 'radiusAll'
 			});
 		})();
-		/* @end */
+		
 	})();
 });
