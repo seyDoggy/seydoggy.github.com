7b0eef76f62e38312eb76af5608d236dffbd6752
Adam Merrifield <macagp@gmail.com>
2013-07-29 13:20:16 -0400

    Flavorite: revamped responsive behaviors as per SNo3 and CRE4MY.

--- libs/themes/rw/Flavorite.js
+++ libs/themes/rw/Flavorite.js
@@ -41,46 +41,56 @@ jQuery(document).ready(function($){
 		var sdNavOptions = (function(){
 			// invoke sdSmartNav
 			$.sdSmartNav();
+			
 			// if mobile
-			if (jq.add('body').width() <= '600' && jq.add('meta[name=viewport]').length) {
-				// remove additional tiers
-				sdNav.tb2.remove();
-				sdNav.tb3.remove();
-				// add link to navigation
-				$('<a href="#toolbar_vertical" title="menu" class="responsiveMenu"></a>').prependTo(siteHeader).css({
-					"background-image":"url(http://seydoggy.github.com/libs/themes/rw/plus.black.32.png)",
-					"background-repeat":"no-repeat",
-					"float":"right",
-					"height":"32px",
-					"margin-top":(siteHeader.height()/2)-16,
-					"width":"32px"
-				});
-				// move nav after footer
-				sdNav.tb1.insertAfter('.footerNormal').attr('id','toolbar_vertical')
-					.css({'display':'block','margin-top':'2em'})
-					.find('a.current').siblings('ul')
-						.css("display", "block")
-						.end().parents("ul").css("display", "block");
-				// capture first and last items for toolbar_vertical
-				sdNav.tb1.find('ul:first li:last a').css('border-style', 'none');
-				sdNav.tb1.find('ul a:first').addClass('firstLink');
-				sdNav.tb1.find('ul:first li:last').addClass('lastListItem');
-				sdNav.tb1.find('ul:first li:last a').addClass('lastLink');
-			} else {
-				// capture first and last items for toolbar_vertical
-				sdNav.tb3.find('ul:first li:last a').css('border-style', 'none');
-				sdNav.tb3.find('ul a:first').addClass('firstLink');
-				sdNav.tb3.find('ul:first li:last').addClass('lastListItem');
-				sdNav.tb3.find('ul:first li:last a').addClass('lastLink');
-			}
+			var responsiveNavHelper = function () {
+				if (jq.add(window).width() <= '600' && jq.add('meta[name=viewport]').length) {
+					// remove additional tiers
+					sdNav.tb2.remove();
+					sdNav.tb3.remove();
+					// add link to navigation
+					if (!$('.responsiveMenu').length) {
+						$('<a href="#toolbar_vertical" title="menu" class="responsiveMenu"></a>').prependTo(siteHeader).css({
+							"background-image":"url(https://d2c8zg9eqwmdau.cloudfront.net/rw/plus.black.32.png)",
+							"background-repeat":"no-repeat",
+							"float":"right",
+							"height":"32px",
+							"margin-top":(siteHeader.height()/2)-16,
+							"width":"32px"
+						});
+					}
+					// move nav after footer
+					sdNav.tb1.insertAfter('.footerNormal').attr('id','toolbar_vertical')
+						.css({'display':'block','margin-top':'2em'})
+						.find('a.current').siblings('ul')
+							.css("display", "block")
+							.end().parents("ul").css("display", "block");
+					// capture first and last items for toolbar_vertical
+					sdNav.tb1.find('ul:first li:last a').css('border-style', 'none');
+					sdNav.tb1.find('ul a:first').addClass('firstLink');
+					sdNav.tb1.find('ul:first li:last').addClass('lastListItem');
+					sdNav.tb1.find('ul:first li:last a').addClass('lastLink');
+				} else {
+					// capture first and last items for toolbar_vertical
+					sdNav.tb3.find('ul:first li:last a').css('border-style', 'none');
+					sdNav.tb3.find('ul a:first').addClass('firstLink');
+					sdNav.tb3.find('ul:first li:last').addClass('lastListItem');
+					sdNav.tb3.find('ul:first li:last a').addClass('lastLink');
+				}
+			};
+
+			responsiveNavHelper();
+
+			$(window).on('resize orientationchange', responsiveNavHelper);
 		})();
+
 
 		// TITLE VERTICAL ALIGNMENT
 		var sdTitleAlign = (function(){
 			logoImg.css('padding-top','0').sdVertAlign(siteHeader);
 			title.css('padding-top','0').sdVertAlign(siteHeader);
 			// if not mobile
-			if (!(jq.add('body').width() <= '600')) {
+			if (!(jq.add(window).width() <= '600')) {
 				sdNav.tb1.css('padding-top','0').sdVertAlign(siteHeader);
 			}
 		})();
